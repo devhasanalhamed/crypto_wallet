77ab5683a368c560e470c25234ae3f233399d302
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-23 16:57:57 +0300

    Create http_service.dart

---
+++ lib/core/services/http_service.dart
@@ -0,0 +1,28 @@
+import 'package:crypto_wallet/core/constants/api_constants.dart';
+import 'package:dio/dio.dart';
+
+class HTTPService {
+  final Dio _dio = Dio();
+
+  HTTPService() {
+    _configureDio();
+  }
+
+  void _configureDio() {
+    _dio.options = BaseOptions(
+      baseUrl: 'https://api.cryptorank.io/v1/',
+      queryParameters: {
+        'api_key': CRYPTO_RANK_API_KEY,
+      },
+    );
+  }
+
+  Future<dynamic> get(String path) async {
+    try {
+      Response response = await _dio.get(path);
+      return response.data;
+    } catch (e) {
+      print('dio get error: $e');
+    }
+  }
+}
