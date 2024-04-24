dded703d1e9fd016d7fd7e95f60acb3997cc76c0
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-24 10:48:18 +0300

    Update main.dart
    insure classes are initialized

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await registerServices();
   await registerControllers();
   runApp(const MyApp());
