7c880b27ad54104b4cee5d398c47991b61c77999
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-24 10:44:17 +0300

    Update register_services.dart

--- lib/core/utils/register_services.dart
+++ lib/core/utils/register_services.dart
@@ -1,8 +1,15 @@
+import 'package:crypto_wallet/app/logic/assets_controller.dart';
 import 'package:crypto_wallet/core/services/http_service.dart';
 import 'package:get/get.dart';
 
 Future<void> registerServices() async {
   Get.put(
     HTTPService(),
+  );
+}
+
+Future<void> registerControllers() async {
+  Get.put(
+    AssetsController(),
   );
 }
